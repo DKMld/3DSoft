caca32b61c72c23800a35d19df5b38219da64d14
Dimitar Mladenov <miti_01@abv.bg>
2023-03-22 13:08:09 +0200

    added_function_to_get_current_user_ip_and_changed_country_and_value

--- common/views.py
+++ common/views.py
@@ -1,10 +1,14 @@
+import requests
 from django.shortcuts import render
 
 
 def home_page(request):
+    current_user_location = get_current_user_location()
 
     context = {
-        'user_is_auth': request.user.is_authenticated
+        'user_is_auth': request.user.is_authenticated,
+        'current_user_location': current_user_location['country']
+
     }
 
     return render(request, 'common/home.html', context)
@@ -13,3 +17,29 @@ def home_page(request):
 def checkout(request):
 
     return render(request, 'common/checkout.html')
+
+
+
+
+
+
+
+
+def get_current_user_ip():
+    response = requests.get(f'https://api64.ipify.org?format=json').json()
+    return response['ip']
+
+
+def get_current_user_location():
+    ip_address = get_current_user_ip()
+
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+
+    location_data = {
+        'ip': ip_address,
+        'city': response.get('city'),
+        'region': response.get('region'),
+        'country': response.get('country_name'),
+    }
+
+    return location_data
